d0b6377ea30a22e89a738dcf9bc09a9506e89b43
JKuriata <120460338+JKuriata@users.noreply.github.com>
2023-01-16 19:47:56 +0000

    finished lesson 5

--- Lesson 5 - Lists.py
+++ Lesson 5 - Lists.py
@@ -34,3 +34,37 @@ print("-------------")
 #Much like variables, lists can be updated.
 bucket_list[2] = "Get a tattoo"
 print(bucket_list)
+print("-------------")
+#But what if you decided, actually you still want to get a second piercing. You can use a method!
+bucket_list.append("Get a piercing")
+print(bucket_list)
+print("-------------")
+#What if after watching your friend get a tattoo you decide maybe it's too painful for you?
+bucket_list.remove("Get a tattoo")
+print(bucket_list)
+print("-------------")
+#There are a good few methods for lists, some of these are -
+    # .remove()
+    # .append()
+    # .reverse()
+    # .sort()
+    # .count()
+    # .extend()
+    # .pop()        (removes last thing on the list)
+
+#Alright, you know the drill, lets put what you've learned to the test!
+
+#Activity 1 
+#Create a list of your favourite website (3 of them), and then add another two once you’ve created the list. Then remove the last website.
+#------------------------------
+#region - Answer 
+fav_sites = ["w3schools", "youtube", "facebook"]
+fav_sites.append("instagram")
+fav_sites.append("ign")
+fav_sites.pop()
+print(fav_sites)
+print("-------------")
+#endregion
+
+#Activity 2
+#Research the listed methods and make an example using each.
